f52aa08f51e624dfdbfe8ed713f645cfaf55f56d
BlackCrystalic <blackcrystalic@inbox.ru>
2024-04-18 19:11:34 +0300

    okay, no webhooks, fuck it

--- bot.js
+++ bot.js
@@ -6,24 +6,11 @@ const client = new Discord.Client({
     disabledEvents: [
         "TYPING_START"
     ],
-    partials: [
-        Discord.Partials.Channel,
-        Discord.Partials.GuildMember,
-        Discord.Partials.Message,
-        Discord.Partials.Reaction,
-        Discord.Partials.User,
-        Discord.Partials.GuildScheduledEvent
-    ],
     intents: [
         Discord.GatewayIntentBits.Guilds,
-        Discord.GatewayIntentBits.GuildMembers,
-        Discord.GatewayIntentBits.GuildIntegrations,
-        Discord.GatewayIntentBits.GuildWebhooks,
-        Discord.GatewayIntentBits.GuildInvites,
         Discord.GatewayIntentBits.GuildMessages,
-        Discord.GatewayIntentBits.GuildMessageTyping,
-        Discord.GatewayIntentBits.GuildScheduledEvents,
-        Discord.GatewayIntentBits.MessageContent
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildMembers
     ],
     restTimeOffset: 0
 });
@@ -61,65 +48,12 @@ client.commands = new Discord.Collection();
 
 process.on('unhandledRejection', error => {
     console.error('Unhandled promise rejection:', error);
-    const embed = new Discord.EmbedBuilder()
-        .setTitle(`🚨・Unhandled promise rejection`)
-        .addFields([
-            {
-                name: "Error",
-                value: error ? Discord.codeBlock(error) : "No error",
-            },
-            {
-                name: "Stack error",
-                value: error.stack ? Discord.codeBlock(error.stack) : "No stack error",
-            }
-        ])
-        .setColor(client.config.colors.normal)
-    global.webhookLogs.send({
-        username: 'Bot Logs',
-        embeds: [embed],
-    }).catch(() => {
-        console.log('Error sending unhandledRejection to webhook')
-        console.log(error)
-    })
 });
 
 process.on('warning', warn => {
     console.warn("Warning:", warn);
-    const embed = new Discord.EmbedBuilder()
-        .setTitle(`🚨・New warning found`)
-        .addFields([
-            {
-                name: `Warn`,
-                value: `\`\`\`${warn}\`\`\``,
-            },
-        ])
-        .setColor(client.config.colors.normal)
-    global.webhookLogs.send({
-        username: 'Bot Logs',
-        embeds: [embed],
-    }).catch(() => {
-        console.log('Error sending warning to webhook')
-        console.log(warn)
-    })
 });
 
 client.on(Discord.ShardEvents.Error, error => {
-    console.log(error)
-    const embed = new Discord.EmbedBuilder()
-        .setTitle(`🚨・A websocket connection encountered an error`)
-        .addFields([
-            {
-                name: `Error`,
-                value: `\`\`\`${error}\`\`\``,
-            },
-            {
-                name: `Stack error`,
-                value: `\`\`\`${error.stack}\`\`\``,
-            }
-        ])
-        .setColor(client.config.colors.normal)
-    global.webhookLogs.send({
-        username: 'Bot Logs',
-        embeds: [embed],
-    });
+    console.log(error);
 });
--- index.js
+++ index.js
@@ -9,11 +9,6 @@ require("./socket/Redis")();
 
 // TODO: Do the auto reconections on drop and some tries to be online with fucked up state plus auto update and restart in future
 
-global.webhookLogs = new Discord.WebhookClient({
-    id: process.env.WEBHOOK_ID,
-    token: process.env.WEBHOOK_TOKEN,
-});
-
 const manager = new Discord.ShardingManager('./bot.js', {
     totalShards: 'auto',
     token: process.env.DISCORD_TOKEN,
@@ -31,85 +26,22 @@ console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), chalk.red(`Ve
 console.log(`\u001b[0m`);
 
 manager.on('shardCreate', shard => {
-    let embed = new Discord.EmbedBuilder()
-        .setTitle(`🆙・Launching shard`)
-        .setDescription(`A shard has just been launched`)
-        .setFields([
-            {
-                name: "🆔┆ID",
-                value: `${shard.id + 1}/${manager.totalShards}`,
-                inline: true
-            },
-            {
-                name: `📃┆State`,
-                value: `Starting up...`,
-                inline: true
-            }
-        ])
-        .setColor(config.colors.normal)
-    global.webhookLogs.send({
-        username: 'Bot Logs',
-        embeds: [embed],
-    });
-
-    console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), (chalk.green(`Starting`)), chalk.red(`Shard #${shard.id + 1}`), (chalk.white(`...`)))
+    console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), (chalk.green(`Starting`)), chalk.red(`Shard #${shard.id + 1}`), (chalk.white(`...`)));
     console.log(`\u001b[0m`);
 
     shard.on("death", (process) => {
-        const embed = new Discord.EmbedBuilder()
-            .setTitle(`🚨・Closing shard ${shard.id + 1}/${manager.totalShards} unexpectedly`)
-            .setFields([
-                {
-                    name: "🆔┆ID",
-                    value: `${shard.id + 1}/${manager.totalShards}`,
-                },
-            ])
-            .setColor(config.colors.normal)
-        global.webhookLogs.send({
-            username: 'Bot Logs',
-            embeds: [embed]
-        });
-
+        console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), (chalk.green(`Death`)), chalk.red(`Shard #${shard.id + 1}`), (chalk.white(`...`)));
         if (process.exitCode === null) {
-            const embed = new Discord.EmbedBuilder()
-                .setTitle(`🚨・Shard ${shard.id + 1}/${manager.totalShards} exited with NULL error code!`)
-                .setFields([
-                    {
-                        name: "PID",
-                        value: `\`${process.pid}\``,
-                    },
-                    {
-                        name: "Exit code",
-                        value: `\`${process.exitCode}\``,
-                    }
-                ])
-                .setColor(config.colors.normal)
-            global.webhookLogs.send({
-                username: 'Bot Logs',
-                embeds: [embed]
-            });
+            console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), (chalk.green(`Exited With NULL`)), chalk.red(`Shard #${shard.id + 1}`), (chalk.white(`...`)));
         }
     });
 
     shard.on("shardDisconnect", (event) => {
-        const embed = new Discord.EmbedBuilder()
-            .setTitle(`🚨・Shard ${shard.id + 1}/${manager.totalShards} disconnected`)
-            .setDescription("Dumping socket close event...")
-            .setColor(config.colors.normal)
-        global.webhookLogs.send({
-            username: 'Bot Logs',
-            embeds: [embed],
-        });
+        console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), (chalk.green(`Disconnected`)), chalk.red(`Shard #${shard.id + 1}`), (chalk.white(`...`)));
     });
 
     shard.on("shardReconnecting", () => {
-        const embed = new Discord.EmbedBuilder()
-            .setTitle(`🚨・Reconnecting shard ${shard.id + 1}/${manager.totalShards}`)
-            .setColor(config.colors.normal)
-        global.webhookLogs.send({
-            username: 'Bot Logs',
-            embeds: [embed],
-        });
+        console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), (chalk.green(`Reconnecting`)), chalk.red(`Shard #${shard.id + 1}`), (chalk.white(`...`)));
     });
 });
 
@@ -117,51 +49,8 @@ manager.spawn();
 
 process.on('unhandledRejection', error => {
     console.error('Unhandled promise rejection:', error);
-    if (error)
-        if (error.length > 950)
-            error = error.slice(0, 950) + '... view console for details';
-    if (error.stack)
-        if (error.stack.length > 950)
-            error.stack = error.stack.slice(0, 950) + '... view console for details';
-    else
-        return
-
-    const embed = new Discord.EmbedBuilder()
-        .setTitle(`🚨・Unhandled promise rejection`)
-        .addFields([
-            {
-                name: "Error",
-                value: error ? Discord.codeBlock(error) : "No error",
-            },
-            {
-                name: "Stack error",
-                value: error.stack ? Discord.codeBlock(error.stack) : "No stack error",
-            }
-        ])
-    global.webhookLogs.send({
-        username: 'Bot Logs',
-        embeds: [embed],
-    }).catch(() => {
-        console.log('Error sending unhandled promise rejection to webhook')
-        console.log(error)
-    })
 });
 
 process.on('warning', warn => {
     console.warn("Warning:", warn);
-    const embed = new Discord.EmbedBuilder()
-        .setTitle(`🚨・New warning found`)
-        .addFields([
-            {
-                name: `Warn`,
-                value: `\`\`\`${warn}\`\`\``,
-            },
-        ])
-    global.webhookLogs.send({
-        username: 'Bot Logs',
-        embeds: [embed],
-    }).catch(() => {
-        console.log('Error sending warning to webhook')
-        console.log(warn)
-    })
 });
