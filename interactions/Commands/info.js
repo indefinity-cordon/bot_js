e1a88eb7eadd8868843ed8c84c8f71074eb7abc3
BlackCrystalic <blackcrystalic@inbox.ru>
2024-04-18 19:35:24 +0300

    return value fix

--- interactions/Commands/info.js
+++ interactions/Commands/info.js
@@ -1,12 +1,14 @@
 const { CommandInteraction, Client } = require('discord.js');
 const { SlashCommandBuilder } = require('discord.js');
 
-let options = [];
+let servers_options = [];
+let servers_reverse_options = [];
 for (const server of global.handling_game_servers) {
-    options.push({
+    servers_options.push({
         name: `${server.server_name}`,
         value: `${server.db_name}`
     });
+    servers_reverse_options[`${server.server_name}`] = `${server.db_name}`; // Fix for discord.js issue, it's not returning right value
 }
 
 module.exports = {
@@ -24,7 +26,7 @@ module.exports = {
                 .setName('server')
                 .setDescription('Select game server')
                 .setRequired(true)
-                .addChoices(...options)
+                .addChoices(...servers_options)
         )
     ,
 
@@ -42,7 +44,7 @@ module.exports = {
             color: `#6d472b`
         }, interaction);
         const user = await interaction.options.getUser('user');
-        let server = await interaction.options.getString('server');
+        let server = servers_reverse_options[await interaction.options.getString('server')];
         console.log(`target server ${server}`)
         const db_discord_link = await new Promise((resolve, reject) => {
             global.database.query("SELECT player_ckey, discord_id, role_rank, stable_rank FROM discord_links WHERE discord_id = ?", [user.id], (err, result) => {
