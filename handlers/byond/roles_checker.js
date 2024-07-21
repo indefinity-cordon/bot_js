7128c9352fa9952d8017496bd4911ad6eecad0cb
Deleted user <ghost>
2024-07-21 21:34:24 +0500

    F

--- handlers/byond/roles_checker.js
+++ handlers/byond/roles_checker.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = async (client) => {
     client.on(Discord.Events.ClientReady, async () => {
-        setTimeout(updateRoles(client), 10000);
+        setTimeout(updateRoles, 10000, client);
         setInterval(
             updateRoles,
             3600000,
