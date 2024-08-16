9f448a40cbd550311eb1763cd8d783bf59d6a424
Deleted user <ghost>
2024-08-17 03:06:25 +0500

    f

---
+++ handlers/functions/admin.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+
+module.exports = async (client) => {
+//    global.handling_commands_actions["admins"] = client.handleServerSelection;
+//    global.handling_commands.push({ label: "Manage TGS", value: "tgs", role_req: "tgs_role_id" });
+}
