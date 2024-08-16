07f4d1017784e58493479665e0655516b0f92b6a
Deleted user <ghost>
2024-08-16 05:36:39 +0500

    no memory leaks here allowed

--- handlers/components/byondapi.js
+++ handlers/components/byondapi.js
@@ -2,6 +2,7 @@ const chalk = require('chalk');
 const net = require('net');
 
 let econn_error_skipper = [];
+let connections_in_proggress = [];
 
 module.exports = (client) => {
     client.prepareByondAPIRequest = async function ({
@@ -20,6 +21,8 @@ module.exports = (client) => {
                     console.log(chalk.blue(chalk.bold(`ByondAPI`)), (chalk.white(`>>`)), chalk.red(`Request`), (chalk.white(`>>`)), chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`Server req lock lowered for target: ${address}:${port}.`))
                 }
                 return;
+            } else if (connections_in_proggress[`${port}:${address}`]) {
+                return; // Удостоверяемся что у нас не будет ошибок из-за частых попыток соединения, когда оно идет слишком долго
             }
             const textEncoder = new TextEncoder();
             const encodedText = textEncoder.encode(request);
@@ -34,10 +37,12 @@ module.exports = (client) => {
                 const client = net.connect(port, address, () => {
                     client.write(packet);
                 });
+                connections_in_proggress[`${port}:${address}`] = client;
                 client.on('data', (data) => {
                     if (data.slice(0, 2).equals(Buffer.from([0x00, 0x83]))) {
                         const size = data.readUInt16BE(2);
                         const response = data.slice(4, 4 + size);
+                        connections_in_proggress[`${port}:${address}`] = null;
                         client.end();
                         const packetType = response[0];
                         if (packetType === 0x2a) {
@@ -49,6 +54,7 @@ module.exports = (client) => {
                             reject('Unknown data code');
                         }
                     } else {
+                        connections_in_proggress[`${port}:${address}`] = null;
                         client.end();
                         console.log(chalk.blue(chalk.bold(`ByondAPI`)), (chalk.white(`>>`)), chalk.red(`Request`), (chalk.white(`>>`)), chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`BYOND server returned invalid data.`));
                         reject('Failed to read data');
@@ -59,6 +65,7 @@ module.exports = (client) => {
                     econn_error_skipper[`${port}:${address}`]['req_attempts'] = 5; // Таймаут на 5 попыток, да бы JS не жаловался на лик озу
                     econn_error_skipper[`${port}:${address}`]['attempts'] = 0;
                     econn_error_skipper[`${port}:${address}`]['locked'] = true;
+                    connections_in_proggress[`${port}:${address}`] = null;
                     client.end();
                     console.log(chalk.blue(chalk.bold(`ByondAPI`)), (chalk.white(`>>`)), chalk.red(`Request`), (chalk.white(`>>`)), chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`Can't connect to ${address}:${port}: ${err.message}`));
                     reject(err);
