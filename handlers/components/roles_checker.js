93c2c30755cb18078508f4006f508e69dcb514b4
Deleted user <ghost>
2024-08-19 13:45:41 +0500

    fix?

--- handlers/components/roles_checker.js
+++ handlers/components/roles_checker.js
@@ -1,3 +1,6 @@
+const Discord = require('discord.js');
+const chalk = require('chalk');
+
 module.exports = async (client) => {
     client.serverRoles = async function ({
         game_server: game_server
@@ -14,27 +17,75 @@ module.exports = async (client) => {
 }
 
 async function updateRoles(client, game_server) {
-    const db_roles = await client.databaseRequest({ database: game_server.game_connection, query: "SELECT role_id, rank_id FROM discord_ranks ORDER BY rank_id", params: []});
-    if ( !db_roles[0] ) return;
-    const guild = await client.guilds.cache.get(game_server.guild);
-    if ( !guild ) return;
-    const members = await guild.members.fetch({ time: 5 * 60 * 1000 }); // Ждем N минут (первое число)
-    if ( !members ) return;
-    const roleMap = new Map();
-    db_roles.forEach(row => {
-        roleMap.set(row.role_id, row.rank_id);
-    });
-    for (const [memberId, member] of members) {
-        let discord_link = await client.databaseRequest({ database: game_server.game_connection, query: "SELECT stable_rank FROM discord_links WHERE discord_id = ?", params: [memberId]});
-        if (discord_link[0]) {
-            let rank_id = discord_link[0].stable_rank;
-            member.roles.cache.forEach(role => {
-                const matchingRankId = roleMap.get(role.id);
-                if (matchingRankId && rank_id < matchingRankId) {
-                    rank_id = matchingRankId;
+    try {
+        console.log(chalk.blue(chalk.bold(`Roles Sync`)), (chalk.white(`>>`)), chalk.blue(`[STARTING]`), (chalk.white(`>>`)), chalk.red(`Prepairing for roles update`));
+        const db_roles = await client.databaseRequest({ database: game_server.game_connection, query: "SELECT role_id, rank_id FROM discord_ranks ORDER BY rank_id", params: [] });
+        if ( !db_roles[0] ) return;
+        const guild = await client.guilds.cache.get(game_server.guild);
+        if ( !guild ) return;
+
+        const roleMap = new Map();
+        db_roles.forEach(row => {
+            roleMap.set(row.role_id, row.rank_id);
+        });
+
+        const discordLinks = await client.databaseRequest({ database: game_server.game_connection, query: "SELECT discord_id, stable_rank FROM discord_links", params: [] });
+        const discordLinksMap = new Map();
+        discordLinks.forEach(link => {
+            discordLinksMap.set(link.discord_id, link.stable_rank);
+        });
+
+        const totalMembers = await guild.memberCount;
+        let processedMembers = 0;
+
+        async function processBatch(membersBatch) {
+            console.log(chalk.blue(chalk.bold(`Roles Sync`)), (chalk.white(`>>`)), chalk.blue(`[PROCESSING]`), (chalk.white(`>>`)), chalk.red(`Members length ${membersBatch.size}`));
+
+            const updates = [];
+            for (const member of membersBatch.values()) {
+                const stable_rank = discordLinksMap.get(member.id);
+                if (stable_rank !== undefined) {
+                    let rank_id = stable_rank;
+
+                    member.roles.cache.forEach(role => {
+                        const roleRankId = roleMap.get(role.id);
+                        if (roleRankId && rank_id < roleRankId) {
+                            rank_id = roleRankId;
+                        }
+                    });
+
+                    if (rank_id !== stable_rank) {
+                        updates.push([{ params: [rank_id, member.id] }]);
+                    }
                 }
-            });
-            client.databaseRequest({ database: game_server.game_connection, query: "UPDATE discord_links SET role_rank = ? WHERE discord_id = ?", params: [rank_id, memberId]});
+            }
+
+            if (updates.length) {
+                for (const update of updates) {
+                    client.databaseRequest({ database: game_server.game_connection, query: "UPDATE discord_links SET role_rank = ? WHERE discord_id = ?", params: update.params });
+                }
+            }
         }
-    };
+
+        async function fetchAndProcessMembers() {
+            let after = null;
+            do {
+                const members = await guild.members.list({ limit: BATCH_SIZE, time: 5 * 60 * 1000, after });
+                if (members.size === 0) break;
+
+                await processBatch(members);
+
+                processedMembers += members.size;
+                console.log(chalk.blue(chalk.bold(`Roles Sync`)), (chalk.white(`>>`)), chalk.blue(`[PROCESSING]`), (chalk.white(`>>`)), chalk.red(`Finished processing: ${processedMembers}/${totalMembers} players batch`));
+
+                after = members.last().id;
+            } while (processedMembers < totalMembers);
+        }
+
+        await fetchAndProcessMembers();
+
+        console.log(chalk.blue(chalk.bold(`Roles Sync`)), (chalk.white(`>>`)), chalk.blue(`[DONE]`), (chalk.white(`>>`)), chalk.red(`Processed total of ${totalMembers} members`));
+    } catch (error) {
+        console.log(chalk.blue(chalk.bold(`Roles Sync`)), (chalk.white(`>>`)), chalk.blue(`[ERROR]`), (chalk.white(`>>`)), chalk.red(`Something went wrong`));
+    }
 };
