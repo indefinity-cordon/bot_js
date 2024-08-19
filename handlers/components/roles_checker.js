7e4a34a0657ea02407c512266d105a1e16f04b04
Deleted user <ghost>
2024-08-19 09:50:33 +0500

    this one solv???

--- handlers/components/roles_checker.js
+++ handlers/components/roles_checker.js
@@ -15,19 +15,23 @@ module.exports = async (client) => {
 
 async function updateRoles(client, game_server) {
     const db_roles = await client.databaseRequest({ database: game_server.game_connection, query: "SELECT role_id, rank_id FROM discord_ranks ORDER BY rank_id", params: []});
-    if (!db_roles[0]) return;
+    if ( !db_roles[0] ) return;
     const guild = await client.guilds.cache.get(game_server.guild);
-    if (!guild) return;
-    const members = await guild.members.fetch();
-    if ( !members) return;
+    if ( !guild ) return;
+    const members = await guild.members.fetch({ time: 5 * 60 * 1000 }); // Ждем N минут (первое число)
+    if ( !members ) return;
+    const roleMap = new Map();
+    db_roles.forEach(row => {
+        roleMap.set(row.role_id, row.rank_id);
+    });
     members.forEach(async (member) => {
         let discord_link = await client.databaseRequest({ database: game_server.game_connection, query: "SELECT stable_rank FROM discord_links WHERE discord_id = ?", params: [member.id]});
         if (discord_link[0]) {
             let rank_id = discord_link[0].stable_rank;
-            member.roles.cache.forEach(async (role) => {
-                const matchingRole = db_roles.find(row => row.role_id === role.id);
-                if (matchingRole && rank_id < matchingRole.rank_id) {
-                    rank_id = matchingRole.rank_id;
+            member.roles.cache.forEach(role => {
+                const matchingRankId = roleMap.get(role.id);
+                if (matchingRankId && rank_id < matchingRankId) {
+                    rank_id = matchingRankId;
                 }
             });
             client.databaseRequest({ database: game_server.game_connection, query: "UPDATE discord_links SET role_rank = ? WHERE discord_id = ?", params: [rank_id, member.id]});
