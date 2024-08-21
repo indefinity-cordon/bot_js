3c3eec7ba77ab2078e880d0524861af7773f9984
Deleted user <ghost>
2024-08-21 05:08:49 +0500

    No pig counter, trying to find reason for funny

--- events/clientReady.js
+++ events/clientReady.js
@@ -6,11 +6,5 @@ module.exports = async (client) => {
     console.log(`\u001b[0m`);
     console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), chalk.red(`Shard #${client.shard.ids[0] + 1}`), chalk.green(`is ready!`));
     console.log(chalk.blue(chalk.bold(`Bot`)), (chalk.white(`>>`)), chalk.green(`Started on`), chalk.red(`${client.guilds.cache.size}`), chalk.green(`servers!`));
-
-    let pigs_count = 0;
-    setInterval(function () {
-        pigs_count++
-        client.user.setPresence({ activities: [{ name: `counting bad devs, ${numberToWords.toWords(pigs_count)}`, type: Discord.ActivityType.Playing }], status: 'online' });
-    })
 }
 
