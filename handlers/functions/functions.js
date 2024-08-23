f65d6a9b22e041c2a42dc6b7648353750314038a
Deleted user <ghost>
2024-08-23 20:29:15 +0500

    some paginated shit

--- handlers/functions/functions.js
+++ handlers/functions/functions.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+const { ActionRowBuilder, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = async (client) => {
     //----------------------------------------------------------------//
@@ -118,6 +118,72 @@ module.exports = async (client) => {
                         desc: `Time ran out! Please try again.`,
                         color: `#c70058`
                     }, interaction);
+                    await interaction.editReply({ components: [] });
+                }
+                resolve();
+            });
+        });
+    };
+
+    client.sendPaginatedSelectMenu = async function (interaction, customId, customDesc, customOptions, customContent, perPage = 25) {
+        const totalPages = Math.ceil(customOptions.length / perPage);
+        const sendPage = async (page) => {
+            const start = page * perPage;
+            const end = start + perPage;
+            const currentOptions = customOptions.slice(start, end);
+            const menu = new StringSelectMenuBuilder()
+                .setCustomId(customId)
+                .setPlaceholder(customDesc)
+                .addOptions(currentOptions);
+            const buttons = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('button_prev')
+                        .setLabel('Previous')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(page === 0),
+                    new ButtonBuilder()
+                        .setCustomId('button_next')
+                        .setLabel('Next')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(page === totalPages - 1)
+                );
+            await interaction.editReply({
+                content: `Page ${page + 1}/${totalPages}: ${customContent}`,
+                components: [new ActionRowBuilder().addComponents(menu), buttons],
+                ephemeral: true
+            });
+        };
+        if (client.activeCollectors && client.activeCollectors[interaction.user.id]) {
+            client.activeCollectors[interaction.user.id].stop();
+        }
+        let currentPage = 0;
+        await sendPage(currentPage);
+        const filter = (i) => ['button_prev', 'button_next'].includes(i.customId) && i.user.id === interaction.user.id;
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
+        client.activeCollectors = client.activeCollectors || {};
+        client.activeCollectors[interaction.user.id] = collector;
+        return new Promise((resolve) => {
+            collector.on('collect', async collected => {
+                if (i.customId === 'button_prev' && currentPage > 0) {
+                    currentPage--;
+                    await sendPage(currentPage);
+                } else if (i.customId === 'button_next' && currentPage < totalPages - 1) {
+                    currentPage++;
+                    await sendPage(currentPage);
+                } else {
+                    await collected.deferUpdate();
+                    resolve(collected.values[0]);
+                }
+            });
+            collector.on('end', async collected => {
+                if (collected.size === 0) {
+                    await client.ephemeralEmbed({
+                        title: `Request`,
+                        desc: `Time ran out! Please try again.`,
+                        color: `#c70058`
+                    }, interaction);
+                    await interaction.editReply({ components: [] });
                 }
                 resolve();
             });
@@ -129,7 +195,7 @@ module.exports = async (client) => {
         const collected = await interaction.channel.awaitMessages({ filter, max: 1, time: 60000, errors: ['time'] });
     
         if (collected.size === 0) {
-            await interaction.followUp({ content: 'Time ran out! Please try again.', ephemeral: true });
+            await interaction.followUp({ content: 'Time ran out! Please try again.', components: [], ephemeral: true });
             return null;
         }
     
