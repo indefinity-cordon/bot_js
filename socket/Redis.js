d576d3604aa61039eb696af3e973b835a3426de6
Deleted user <ghost>
2024-08-30 00:53:44 +0500

    d

--- socket/Redis.js
+++ socket/Redis.js
@@ -7,13 +7,6 @@ connection.on('error', err => console.log(chalk.blue(chalk.bold(`Socket`)), chal
 
 module.exports = async (client) => {
     console.log(chalk.blue(chalk.bold(`Socket`)), chalk.white(`>>`), chalk.red(`Redis`), chalk.green(`starting connecting...`));
-    client.redis_connection = null;
-    await connection.connect((err, result) => {
-        if (err) {
-            console.log(chalk.blue(chalk.bold(`Socket`)), chalk.white(`>>`), chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`Redis`), chalk.red(`Failed connect to the Redis server.`));
-        } else {
-            client.redis_connection = connection;
-            console.log(chalk.blue(chalk.bold(`Socket`)), chalk.white(`>>`), chalk.red(`Redis`), chalk.green(`Connected to the Redis server.`));
-        }
-    });
+    await connection.connect();
+    client.redis_connection = connection;
 }
