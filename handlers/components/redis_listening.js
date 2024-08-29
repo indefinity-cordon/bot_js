bc88358cc3a82b7d724674d0d45b7a19026a41a4
Deleted user <ghost>
2024-08-30 02:52:47 +0500

    f

--- handlers/components/redis_listening.js
+++ handlers/components/redis_listening.js
@@ -26,4 +26,26 @@ async function startListining(client) {
             client.redisCallback(JSON.parse(data));
         }
     });
+
+    for (const server_name in client.servers_link) {
+        game_server = client.servers_link[server_name];
+        const db_request = await client.databaseRequest(client.database, "SELECT type, channel_id, message_id FROM server_channels WHERE server_name = ? AND message_id == \"-2\"", [game_server.server_name]);
+        for (const message_collector of db_request) {
+            const channel = await client.channels.fetch(message_collector.channel_id);
+            if (!channel) return;
+            channel.createMessageCollector().on('collect', message => {
+                sendToRedis(message, client, message_collector.type);
+            });
+        }
+    }
+}
+
+function sendToRedis(message, client, redis_channel) {
+    const redisMessage = {
+        color: message.member.roles.highest.hexColor,
+        source: "DISCORD",
+        author: message.member.displayName,
+        message: message.content
+    };
+    client.redis_connection.publish(redis_channel, JSON.stringify(redisMessage));
 }
