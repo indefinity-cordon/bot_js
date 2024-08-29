17d7500092133080c6da67dc679924f011c1f3a1
Deleted user <ghost>
2024-08-30 00:40:02 +0500

    f

--- socket/Redis.js
+++ socket/Redis.js
@@ -1,14 +1,7 @@
 const redis = require('redis');
 const chalk = require('chalk');
 
-const connection = redis.createClient({
-    username: `${process.env.REDIS_USER}`,
-    password: `${process.env.REDIS_PASSWORD}`,
-    socket: {
-        host: `${process.env.REDIS_SERVER}`,
-        port: `${process.env.REDIS_PORT}`
-    }
-});
+const connection = redis.createClient(process.env.REDIS_STRING);
 
 connection.on('error', err => console.log(chalk.blue(chalk.bold(`Socket`)), chalk.white(`>>`), chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`Redis`), chalk.red(err)));
 
