8d87589d3d64e94a2de50f4e16d674d104d453e5
Deleted user <ghost>
2024-08-30 02:56:42 +0500

    а

--- handlers/components/redis_listening.js
+++ handlers/components/redis_listening.js
@@ -29,7 +29,7 @@ async function startListining(client) {
 
     for (const server_name in client.servers_link) {
         game_server = client.servers_link[server_name];
-        const db_request = await client.databaseRequest(client.database, "SELECT type, channel_id, message_id FROM server_channels WHERE server_name = ? AND message_id == \"-2\"", [game_server.server_name]);
+        const db_request = await client.databaseRequest(client.database, "SELECT type, channel_id, message_id FROM server_channels WHERE server_name = ? AND message_id = \"-2\"", [game_server.server_name]);
         for (const message_collector of db_request) {
             const channel = await client.channels.fetch(message_collector.channel_id);
             if (!channel) return;
