77ee915d0f0ae77ac885ac79fb53806f53c69716
Deleted user <ghost>
2024-08-30 03:11:18 +0500

    some fun around

--- handlers/components/redis_listening.js
+++ handlers/components/redis_listening.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 
 let subscriber;
+let collectors = [];
 
 module.exports = async (client) => {
     client.on(Discord.Events.ClientReady, async () => {
@@ -27,15 +28,19 @@ async function startListining(client) {
         }
     });
 
+    for (const old_collector of collectors) {
+        old_collector.stop();
+    }
     for (const server_name in client.servers_link) {
-        game_server = client.servers_link[server_name];
-        const db_request = await client.databaseRequest(client.database, "SELECT type, channel_id, message_id FROM server_channels WHERE server_name = ? AND message_id = \"-2\"", [game_server.server_name]);
+        const db_request = await client.databaseRequest(client.database, "SELECT type, channel_id, message_id FROM server_channels WHERE server_name = ? AND message_id = \"-2\"", [client.servers_link[server_name].server_name]);
         for (const message_collector of db_request) {
             const channel = await client.channels.fetch(message_collector.channel_id);
             if (!channel) return;
-            channel.createMessageCollector().on('collect', message => {
+            const collector = channel.createMessageCollector()
+            collector.on('collect', message => {
                 sendToRedis(message, client, message_collector.type);
             });
+            collectors.push(collector)
         }
     }
 }
