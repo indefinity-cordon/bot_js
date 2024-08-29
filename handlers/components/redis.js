92669bc447304d7bccb30aeb317190cecf253a92
Deleted user <ghost>
2024-08-30 01:47:59 +0500

    F

--- handlers/components/redis.js
+++ handlers/components/redis.js
@@ -24,7 +24,7 @@ module.exports = (client) => {
         }
 
         if (!responded_game_server) {
-            console.log(chalk.blue(chalk.bold(`Socket`)), chalk.white(`>>`), chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`Redis`), chalk.red(`Failed to find server object. Aborting. data: ${JSON.stringify(data)}, found objects: ${JSON.stringify(client.servers_link)}`));
+            console.log(chalk.blue(chalk.bold(`Socket`)), chalk.white(`>>`), chalk.red(`[ERROR]`), chalk.white(`>>`), chalk.red(`Redis`), chalk.red(`Failed to find server object. Aborting. data: ${data.source}, found objects: ${JSON.stringify(client.servers_link)}`));
             return;
         }
 
