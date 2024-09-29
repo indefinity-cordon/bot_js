58321e53c5e3fb082caf88f2633ca33893b46e51
Deleted user <ghost>
2024-09-29 17:16:37 +0500

    one more qol change

--- server_modules/servers/cm.js
+++ server_modules/servers/cm.js
@@ -30,7 +30,7 @@ module.exports = async (client, game_server) => {
                 }, message);
             }
         } catch (error) {
-            if (game_server.handle_status) game_server.handle_status(false);
+            game_server.handle_status(false);
             game_server.online = 0
             for (const message of game_server.updater_messages[type]) {
                 await client.sendEmbed({
