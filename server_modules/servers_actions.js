1369f222432090c36518de7e778db1fd71c45732
Deleted user <ghost>
2024-10-05 18:46:59 +0500

    f

--- server_modules/servers_actions.js
+++ server_modules/servers_actions.js
@@ -31,12 +31,12 @@ module.exports = async (client) => {
                 });
             } else {
                 server.sync();
-                const data = await client.tgs_getInstance();
-                server.instance_name = data.content.name;
                 for (const guild_id in global.guilds_link) {
                     if (global.guilds_link[guild_id].id !== server.data.guild) continue;
                     game_server.discord_server = global.guilds_link[guild_id]
                 }
+                const data = await client.tgs_getInstance(game_server.discord_server.settings_data.tgs_address);
+                server.instance_name = data.content.name;
                 if (server.data.db_connection_string) {
                     server.game_connection = await global.mysqlCreate(server.data.db_connection_string);
                 }
