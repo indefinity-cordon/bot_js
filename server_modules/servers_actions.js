db7d49c4903dc9955cefc50d0c4fceab01dd4b5e
Deleted user <ghost>
2024-10-05 19:42:02 +0500

    syka blyat

--- server_modules/servers_actions.js
+++ server_modules/servers_actions.js
@@ -37,6 +37,7 @@ module.exports = async (client) => {
                 }
                 if (server.discord_server.settings_data.tgs_address) {
                     const data = await client.tgs_getInstance(server.discord_server.settings_data.tgs_address.data.setting, server.data.tgs_id);
+                    console.log(data, data.content)
                     server.instance_name = data.content.name;
                 }
                 if (server.data.db_connection_string) {
